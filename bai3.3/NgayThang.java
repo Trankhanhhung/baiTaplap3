d744e3638ec8cb2ca60c1c3b3dca2242d91b9201
Trankhanhhung <125333951+Trankhanhhung@users.noreply.github.com>
2023-04-20 14:47:53 +0700

    Add files via upload

---
+++ bai3.3/NgayThang.java
@@ -0,0 +1,9 @@
+public class NgayThang {
+    int ngay, thang, nam;
+
+    public NgayThang(int ngay, int thang, int nam) {
+        this.ngay = ngay;
+        this.thang = thang;
+        this.nam = nam;
+    }
+}
